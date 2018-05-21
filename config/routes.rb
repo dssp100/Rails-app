e148f643abebe838a261e9131b5303d7ef6aed55
Daniel Springer <dssp@freenet.de>
2018-05-21 19:25:40 -0300

    3.8 add description of change. Fixed route issue.

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ root 'simple_pages#index'
   get 'simple_pages/contact'
   get 'simple_pages/index'
 
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
